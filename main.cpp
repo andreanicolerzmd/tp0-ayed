1a1cfbecde0288281bfbf664060cebea562fc315
andreanicolerzmd <aranzamendiandreanicole@gmail.com>
2026-04-29 17:22:00 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hola mundo" << endl;
+    cout << "Andrea Aranzamendi" << endl;
+    cout << "AyED K1053" << endl;
+    cout << "Grupo 10" << endl;
+    return 0;
+}
